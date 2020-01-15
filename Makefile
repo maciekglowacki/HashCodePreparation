6ad6cad012bd8463377d996aca5d2139f3b6ec4a
maciekglowacki <glowackimaciej97@gmail.com>
2020-01-15 11:36:53 +0100

    Create repo for HashCode and prepare parser for first algorithmic problem.

---
+++ Makefile
@@ -0,0 +1,6 @@
+test:
+	python main.py a_example.txt  
+	python main.py b_lovely_landscapes.txt
+	python main.py c_memorable_moments.txt
+	python main.py d_pet_pictures.txt  
+	python main.py e_shiny_selfies.txt
---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python
+from dataclasses import dataclass
+from typing import List
+import os
+import sys
+photosList = []
+allTagsCount = 0
+
+pathToFile = os.getcwd() + "/QualificationRound2019.in/" + sys.argv[1]
+
+@dataclass
+class Photo:
+    position: str
+    tagsCount: int
+    tags: List[str]
+    
+    def AddTag(self,tag):
+        self.tags.append(tag)
+
+class Slide: 
+    def __init__(self,slide):
+        self.slide = slide
+    
+    def CheckPosition(self):
+        pass
+
+
+
+with open(pathToFile) as fileObject:
+    photosCount = int(fileObject.readline())
+    #print(photosCount)
+    for i in range(photosCount):
+        photo = fileObject.readline().rstrip().split(' ')
+        
+        tagsCount = int(len(photo[2:]))
+        position = photo[0]
+        
+        photosList.append(Photo(position,tagsCount,[]))
+        allTagsCount += tagsCount
+       # print(f'Tags count: {tagsCount}')
+        for j in range(tagsCount):
+            tag = photo[j+2].rstrip()
+            #print(f'Tag: {tag}', end= " ")
+            photosList[i].AddTag(tag)
+
+#for x in photosList:
+   #print(f'Tags count: {x.tagsCount}')
+   #print(f'Tags list: {x.tags}')
+
+print(f'Photos count: {len(photosList)}')
+print(f'All tags count: {allTagsCount}')
+print(f'Average tags per photo: {allTagsCount/len(photosList)}')
